763170eead8310114ac2bbc8e8257ddbd2f17a1c
Yuliya Mileeva <prosto.nabor.bukov@gmail.com>
2020-10-28 22:24:59 +0300

    решение задач 1-module

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+
+  let itog = 1;
+
+  if (n === 0) return 1;
+
+  for  (let i = 1; i <= n; i++) {
+    itog = itog * i;
+  }
+
+  return itog;
 }
